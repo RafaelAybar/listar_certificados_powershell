3082477864c960ab8adb1c9a88efb59d90a4ad61
Your Name <you@example.com>
2021-02-20 03:10:20 +0100

    Terminado

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # listar_certificados_powershell
+
+## Descripción
+
+Es importante tener controlados los certificados, así como las CA instaladas, para ello simplificaremos 
+
+la tarea con este script.
+
+## Ejecución
+Creamos la carpeta evidencias con el comando `mkdir evidencias` y luego
+`.\listarcertificados.ps1 > evidencias\fichero.txt` 
--- listarcertificados.ps1
+++ listarcertificados.ps1
@@ -1,3 +1,12 @@
-Write-Output "Se van a listar los certificados de usuarios"
-$usuarios = Get-LocalUser -Name * | Where-Object "Enabled" -eq True | Select Name -ExpandProperty Name
-Write-Output $usuarios 
+Write-Output "Usuarios activos:"
+$usuarios = Get-LocalUser -Name * | Where-Object "Enabled" -eq True | Select-Object Name -ExpandProperty Name
+Write-Output $usuarios
+
+# Comprobamos los certificados en directorios por defecto del usuario
+Write-Output "Certificados de usuario:"
+$certificadosUsuarios = Get-ChildItem Cert:\CurrentUser\ | Format-Table Subject, FriendlyName, Thumbprint
+Write-Output $certificadosUsuarios
+
+# Comprobamos las CA instaladas:
+Write-Output "CA Instaladas"
+Get-ChildItem Microsoft.PowerShell.Security\Certificate::LocalMachine\Root | Format-Table Subject, FriendlyName, Thumbprint
